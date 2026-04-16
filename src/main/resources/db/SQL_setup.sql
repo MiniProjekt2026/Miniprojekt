b4f0437edbcf40ece18347e680a7dcb2f6ad66b1
MagnusNH <Magn763j@stud.ek.dk>
2026-04-16 10:34:24 +0200

    Har tilføjet update wishes og lavet html

--- src/main/resources/db/SQL_setup.sql
+++ src/main/resources/db/SQL_setup.sql
@@ -10,12 +10,20 @@ SET
 FOREIGN_KEY_CHECKS = 0;
 drop table if exists wish_list_tag;
 drop table if exists tag;
+drop table if exists wish;
 drop table if exists wish_list;
 drop table if exists user;
 SET
 FOREIGN_KEY_CHECKS = 1;
 
-create table wish_list
+create table user
+(
+    user_id  int AUTO_INCREMENT primary key,
+    username VARCHAR(15) NOT NULL,
+    password VARCHAR(25) NOT NULL
+);
+
+create table wish
 (
     wish_id      int AUTO_INCREMENT primary key,
     name         VARCHAR(255) not null,
@@ -23,6 +31,7 @@ create table wish_list
     price double,
     quantity     int,
     product_link VARCHAR(255),
+    user_id      int,
     foreign key (user_id) REFERENCES user (user_id) ON DELETE CASCADE
 );
 
@@ -34,17 +43,20 @@ create table tag
 
 create table wish_list_tag
 (
-    wish_id int AUTO_INCREMENT primary key,
-    tag_id  int AUTO_INCREMENT primary key,
-    foreign key (wish_id) REFERENCES wish_list (wish_id) ON DELETE CASCADE,
+    wish_id int,
+    tag_id  int,
+    primary key (wish_id, tag_id),
+    foreign key (wish_id) REFERENCES wish (wish_id) ON DELETE CASCADE,
     foreign key (tag_id) REFERENCES tag (tag_id) ON DELETE CASCADE
 );
 
-create table user
+create table wish_list
 (
-    user_id  int AUTO_INCREMENT primary key,
-    username VARCHAR(15) NOT NULL,
-    password VARCHAR(25) NOT NULL
+    wish_list_id int auto_increment primary key,
+    user_id      int,
+    name         VARCHAR(255) not null,
+    foreign key (user_id) REFERENCES user (user_id) ON DELETE CASCADE
 );
+
 
 
